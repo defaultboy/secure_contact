e3c3f09ea72e0788e15bc4557e9092521603a5ef
defaultboy <mj.muchlinski@gmail.com>
2018-11-04 17:09:10 +0000

    back to top button added

--- js/main.js
+++ js/main.js
@@ -3,6 +3,28 @@ $(document).ready(function(){
 
     $('.how-to-send-card').addClass('unavailable');
 
+    if ($('#back-to-top').length) {
+        var scrollTrigger = 600, // px
+            backToTop = function () {
+                var scrollTop = $(window).scrollTop();
+                if (scrollTop > scrollTrigger) {
+                    $('#back-to-top').addClass('show');
+                } else {
+                    $('#back-to-top').removeClass('show');
+                }
+            };
+        backToTop();
+        $(window).on('scroll', function () {
+            backToTop();
+        });
+        $('#back-to-top').on('click', function (e) {
+            e.preventDefault();
+            $('html,body').animate({
+                scrollTop: 0
+            }, 700);
+        });
+    }   
+
     // steps
     // handle click
     $('.step-list-item').on('click', function(){
